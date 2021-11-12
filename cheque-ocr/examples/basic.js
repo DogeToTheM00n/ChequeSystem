03f07138fbc18b2dcdc49509f0ae70cda48aa6a4
Alankrit <bcs_2019005@iiitm.ac.in>
2021-11-12 20:48:06 +0530

    commit

--- cheque-ocr/examples/basic.js
+++ cheque-ocr/examples/basic.js
@@ -2,7 +2,8 @@ const express = require('express')
 const https = require('https');
 const multer = require('multer');
 const axios = require('axios');
-
+var cors = require('cors')
+app.use(cors())
 
 var fs = require('fs'),
   chequeOCR = require('../index');
@@ -13,29 +14,28 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const upload = multer()
 
-
 // var image = fs.readFileSync('./test/fixtures/sample_cibc.jpg');
 
 function convertToImage(base64) {
   return new Promise(resolve => {
           // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-          var bitmap = new Buffer(base64, 'base64');
+          // var bitmap = new Buffer(base64, 'base64');
+          var bitmap = Buffer.from(base64, 'base64');
           // write buffer to file
           resolve(bitmap)
-      
-
   })
-  
 }
 
 
 
 app.post('/secret/ocr', upload.any(), async (req, res) => {
+
+  // var image = fs.readFileSync('./test/fixtures/sample_cibc.jpg');
   console.log("Attempting to OCR a sample cheque image...");
   var img = await convertToImage(req.body.image);
-  console.log(img)
+  console.log(image)
  // console.log(req.files)
-  chequeOCR(img, function (err, result) {
+  chequeOCR(image, function (err, result) {
     if (err) {
       console.warn("Something went wrong:");
       console.warn(err);
@@ -47,9 +47,6 @@ app.post('/secret/ocr', upload.any(), async (req, res) => {
     }
   });
 })
-
-
-
 
 
 app.listen('9000', (err) => {
