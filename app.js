be443a285ef9f4f4d693b5a757b1a43d8981bcc5
pranav-msc <pranavkotgire1201@gmail.com>
2021-11-14 21:52:50 +0530

    [abcdefgh]

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static('./frontend/build'))
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+//app.use(cors());
 const upload = multer();
 
 //console.log(process.env.PUBLIC_KEY.replace(/\\n/g, '\n'))
@@ -129,7 +129,7 @@ app.post("/api/verifyCheque", jwtHelper.authenticateToken, (req, res) => {
 // })
 
 app.get("*",(req,res)=>{
-  res.sendFile('./frontend/build/index.')
+  res.sendFile('./frontend/build/index.html')
   })
   
 
